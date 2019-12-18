9be18cea2024e5c5bee506814976ae272b57167f
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2019-12-18 18:06:22 -0300

    feat: scroll infinty

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,116 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {Component} from 'react';
+import {ActivityIndicator} from 'react-native';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function User({navigation}) {
-    console.tron.log(navigation.getParam('user'));
-    return <View />;
+import {
+    Container,
+    Header,
+    Avatar,
+    Name,
+    Bio,
+    Stars,
+    Starred,
+    OwnerAvatar,
+    Info,
+    Title,
+    Author,
+    Loading,
+} from './styles';
+
+export default class User extends Component {
+    static navigationOptions = ({navigation}) => ({
+        title: navigation.getParam('user').name,
+    });
+
+    static PropTypes = {
+        navigation: PropTypes.shape({
+            getParam: PropTypes.func,
+        }).isRequired,
+    };
+
+    state = {
+        starts: [],
+        page: 1,
+        per_page: 15,
+        loading: false,
+    };
+
+    async componentDidMount() {
+        const {navigation} = this.props;
+        const user = navigation.getParam('user');
+        const {page, per_page, loading} = this.state;
+
+        const response = await api.get(`/users/${user.login}/starred`, {
+            params: {
+                page,
+                per_page,
+            },
+        });
+        this.setState({starts: response.data, page: page + 1});
+    }
+
+    onEndReached = async () => {
+        const {navigation} = this.props;
+        const user = navigation.getParam('user');
+        const {per_page, page} = this.state;
+
+        const response = await api.get(`/users/${user.login}/starred`, {
+            params: {
+                page,
+                per_page,
+            },
+        });
+
+        this.setState({
+            starts: [...this.state.starts, ...response.data],
+            page: page + 1,
+        });
+    };
+
+    renderFooter = () => {
+        const {loading} = this.state;
+
+        if (!loading) return null;
+        return (
+            <Loading>
+                <ActivityIndicator />
+            </Loading>
+        );
+    };
+
+    render() {
+        const {navigation} = this.props;
+        const {starts} = this.state;
+        const user = navigation.getParam('user');
+
+        return (
+            <Container>
+                <Header>
+                    <Avatar source={{uri: user.avatar}} />
+                    <Name>{user.name}</Name>
+                    <Bio>{user.bio}</Bio>
+                </Header>
+                <Stars
+                    data={starts}
+                    keyExtractor={star => String(star.id)}
+                    renderItem={({item}) => (
+                        <Starred>
+                            <OwnerAvatar
+                                source={{uri: item.owner.avatar_url}}
+                            />
+                            <Info>
+                                <Title>{item.name}</Title>
+                                <Author>{item.owner.login}</Author>
+                            </Info>
+                        </Starred>
+                    )}
+                    onEndReached={() => this.onEndReached()}
+                    onEndReachedThreshold={0.1} //10%
+                    ListFooterComponent={this.renderFooter}
+                />
+            </Container>
+        );
+    }
 }
