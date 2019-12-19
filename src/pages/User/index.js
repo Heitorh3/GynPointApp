bfe8ec7b33fae6e2969130e32d16f00ca1205093
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2019-12-19 09:53:31 -0300

    issue: Warning proptypes navigation

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {ActivityIndicator} from 'react-native';
+import {ActivityIndicator, TouchableOpacity, View} from 'react-native';
 import PropTypes from 'prop-types';
+import {WebView} from 'react-native-webview';
 
 import api from '../../services/api';
 
@@ -20,15 +21,15 @@ import {
 } from './styles';
 
 export default class User extends Component {
-    static navigationOptions = ({navigation}) => ({
-        title: navigation.getParam('user').name,
-    });
-
-    static PropTypes = {
+    static propTypes = {
         navigation: PropTypes.shape({
             getParam: PropTypes.func,
         }).isRequired,
     };
+
+    static navigationOptions = ({navigation}) => ({
+        title: navigation.getParam('user').name,
+    });
 
     state = {
         starts: [],
@@ -80,6 +81,15 @@ export default class User extends Component {
         );
     };
 
+    handleShowItem = html_url => {
+        //console.tron.log(url);
+        return (
+            <View>
+                <WebView source={{uri: html_url}} />;
+            </View>
+        );
+    };
+
     render() {
         const {navigation} = this.props;
         const {starts} = this.state;
@@ -96,15 +106,18 @@ export default class User extends Component {
                     data={starts}
                     keyExtractor={star => String(star.id)}
                     renderItem={({item}) => (
-                        <Starred>
-                            <OwnerAvatar
-                                source={{uri: item.owner.avatar_url}}
-                            />
-                            <Info>
-                                <Title>{item.name}</Title>
-                                <Author>{item.owner.login}</Author>
-                            </Info>
-                        </Starred>
+                        <TouchableOpacity
+                            onPress={() => this.handleShowItem(item.html_url)}>
+                            <Starred>
+                                <OwnerAvatar
+                                    source={{uri: item.owner.avatar_url}}
+                                />
+                                <Info>
+                                    <Title>{item.name}</Title>
+                                    <Author>{item.owner.login}</Author>
+                                </Info>
+                            </Starred>
+                        </TouchableOpacity>
                     )}
                     onEndReached={() => this.onEndReached()}
                     onEndReachedThreshold={0.1} //10%
